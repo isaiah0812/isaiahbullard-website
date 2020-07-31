6b3b3b0e5df20a81d7dc53899b930107691dc59a
Isaiah Bullard <Isaiah_Bullard@baylor.edu>
2020-07-30 21:51:13 -0500

    Adding single embeded players

--- src/pages/projects/components/singleDiv.js
+++ src/pages/projects/components/singleDiv.js
@@ -19,6 +19,10 @@ const SingleFeatures = styled.h5`
   font-weight: normal;
 `
 
+const SingleDescription = styled.p`
+  line-height: normal;
+`
+
 export default ({single}) => {
   return (
     <Container style={{
@@ -40,7 +44,12 @@ export default ({single}) => {
             else return feature + ", "
           })}</SingleFeatures>
         )}
-        <p>{single.description}</p>
+        <SingleDescription>{single.description}</SingleDescription>
+        <p>Release Date: {single.releaseDate}</p>
+        <iframe src="https://open.spotify.com/embed/track/1fZHvbksavmt5XEtAmTBCR" width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Spotify"></iframe>
+        <iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="150" style={{width: '100%  ', maxWidth: '660px', overflow: 'hidden', background: 'transparent'}} sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/evil-plan-feat-louiev-t-fuze/1506525081?i=1506525082" title="Apple Music"></iframe>
+        <iframe style={{border: 0, width: '100%', height: '120px'}} src="https://bandcamp.com/EmbeddedPlayer/track=2329859596/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless title="Bandcamp"><a href="http://isaiahbullard.bandcamp.com/track/evil-plan-feat-louiev-t-fuze">Evil Plan (feat. LouieV T &amp; FUZE) by Isaiah Bullard</a></iframe>
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/795101182&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" title="Soundcloud"></iframe>
       </Container>
     </Container>
   )
