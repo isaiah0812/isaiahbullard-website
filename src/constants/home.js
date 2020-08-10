12c87cb4c5b7c6534a09aa300e058390aabb5144
Isaiah Bullard <Isaiah_Bullard@baylor.edu>
2020-08-09 23:23:09 -0500

    moving home sections to constants folder

---
+++ src/constants/home.js
@@ -0,0 +1,26 @@
+export const sections = [
+    {
+      title: 'About Me',
+      description: "Producer. Songwriter. Wannabe composer. That's me! I'm open to all serious inquiries that involve music. Hit the button below to contact me and let's get locked in!",
+      buttonText: 'Contact',
+      href: '/contact',
+    },
+    {
+      title: 'Music',
+      description: "All of my music is available for digital streaming and download. Check out my entire discography and get more information about each project!",
+      buttonText: 'More',
+      href: '/projects',
+    },
+    {
+      title: 'Beats',
+      description: "I sell beats! Check out my latest beats and all of my beat tapes here. Everything on this page is for sale, so be sure to contact me if you're looking to work together!",
+      buttonText: 'Listen',
+      href: '/beats',
+    },
+    {
+      title: 'Merch',
+      description: "Merchandise coming soon.",
+      buttonText: 'Shop',
+      href: '/merchandise',
+    },
+  ]
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,49 +2,21 @@ import React from 'react';
 import ProjectCarousel from './components/projectCarousel';
 import Container from 'react-bootstrap/container';
 import HomeSection from './components/homeSection';
+import { sections } from '../../constants/home';
 
-const sections = [
-  {
-    title: 'About Me',
-    description: "Producer. Songwriter. Wannabe composer. That's me! I'm open to all serious inquiries that involve music. Hit the button below to contact me and let's get locked in!",
-    buttonText: 'Contact',
-    href: '/contact',
-  },
-  {
-    title: 'Music',
-    description: "All of my music is available for digital streaming and download. Check out my entire discography and get more information about each project!",
-    buttonText: 'More',
-    href: '/projects',
-  },
-  {
-    title: 'Beats',
-    description: "I sell beats! Check out my latest beats and all of my beat tapes here. Everything on this page is for sale, so be sure to contact me if you're looking to work together!",
-    buttonText: 'Listen',
-    href: '/beats',
-  },
-  {
-    title: 'Merch',
-    description: "Merchandise coming soon.",
-    buttonText: 'Shop',
-    href: '/merchandise',
-  },
-]
-
-export default class Home extends React.Component {
-  render () {
-    return (
-      <Container fluid style={{padding: 0}}>
-        <ProjectCarousel />
-        {sections.map((section, index) => (
-          <HomeSection 
-            primary={index%2 === 0}
-            title={section.title}
-            description={section.description}
-            buttonText={section.buttonText}
-            href={section.href}
-          />
-        ))}
-      </Container>
-    );
-  }
+export default () => {
+  return (
+    <Container fluid style={{padding: 0}}>
+      <ProjectCarousel />
+      {sections.map((section, index) => (
+        <HomeSection 
+          primary={index%2 === 0}
+          title={section.title}
+          description={section.description}
+          buttonText={section.buttonText}
+          href={section.href}
+        />
+      ))}
+    </Container>
+  );
 }
