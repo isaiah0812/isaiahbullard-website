73e4f082b0cc40c8f4319e36f5f1c0cab38bdf42
Isaiah Bullard <Isaiah_Bullard@baylor.edu>
2021-03-07 17:07:18 -0600

    adding comments for new classes

--- src/pages/projects/components/youtubeCard.js
+++ src/pages/projects/components/youtubeCard.js
@@ -4,6 +4,13 @@ import styled from 'styled-components';
 
 import { darkBlue, silver, youtubeRed } from '../../../constants/colors';
 
+/**
+ * The container where a music video lives.
+ * @constant
+ * @name YouTubeVideo
+ * @type {import('styled-components').StyledComponent}
+ * @example <YouTubeVideo>...</YouTubeVideo>
+ */
 const YouTubeVideo = styled(Container)`
   width: 60%;
   height: 492px;
@@ -16,6 +23,13 @@ const YouTubeVideo = styled(Container)`
   }
 `
 
+/**
+ * Card that leads to a project page or a single's information
+ * @name YoutubeCard
+ * @author Isaiah Bullard
+ * @version 1.1.0
+ * @example <YoutubeCard title="Title" youtube="FIT_DhSPpyw" />
+ */
 export default class YoutubeCard extends React.Component {
   render() {
     const {
